da85b467fb67ebb081f8c1aa3ffaf296185369c3
marianabarreto84 <marianaportobarreto@gmail.com>
2021-11-15 01:24:47 -0300

    Adicionando .gitignore

--- tabulando/tabulando/settings.py
+++ tabulando/tabulando/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2!-dbl6wo6i!!x@-6k6^8*i)i1i=5w-h9+@lzx*frhkdsls%pu'
+SECRET_KEY = os.environ.get("TABULANDO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
