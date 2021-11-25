5b7ee1c60f792ac082f760e5229e2a201d3794d3
marianabarreto84 <marianaportobarreto@gmail.com>
2021-11-24 23:18:30 -0300

    Alterações no layout, favicon e página de Login

--- tabulando/tabulando/urls.py
+++ tabulando/tabulando/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
 from . import views
 
+
 urlpatterns = [
+    path('', views.pagina_inicial, name='pagina_inicial'),
     path('admin/', admin.site.urls),
-    path('', views.pagina_inicial, name="paginal_inicial"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
